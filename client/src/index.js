9839679aa222c0473ec5aef915797b11123e9521
ChanhoKim <chanhokim9848@gmail.com>
2026-01-09 19:07:17 +0900

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import Authors from "./pages/Authors";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Logout from "./pages/Logout";
+
+// browser router setup
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts/:id", element: <PostDetail /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      { path: "profile/:id", element: <UserProfile /> },
+      { path: "authors", element: <Authors /> },
+      { path: "create", element: <CreatePost /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
+      { path: "logout", element: <Logout /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
