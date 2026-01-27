1f5fd0361543c21efc5cb818fcd9a9b7e5e819e0
ChanhoKim <chanhokim9848@gmail.com>
2026-01-27 10:01:04 +0900

    Incomplete error instantiation in catch block fixed

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -71,7 +71,7 @@ const loginUser = async (req, res, next) => {
     });
     res.status(200).json({ token, id, name });
   } catch (error) {
-    return next(new HttpError());
+    return next(new HttpError("Login failed. Please try again later.", 500));
   }
 };
 
