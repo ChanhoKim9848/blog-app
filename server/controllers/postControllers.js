9fc030c850be2a80fd4955ad6ff1028330551e79
ChanhoKim <chanhokim9848@gmail.com>
2026-02-05 10:12:53 +0900

    create post functionality

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -9,7 +9,65 @@ const HttpError = require("../models/errorModel");
 // POST: api/posts
 // PROTECTED
 const createPost = async (req, res, next) => {
-  res.json("Create Post");
+  try {
+    const { title, category, description } = req.body;
+
+    if (!title || !category || !description || !req.files) {
+      return next(
+        new HttpError("Fill in all fields and choose thumbnail.", 422),
+      );
+    }
+
+    const { thumbnail } = req.files;
+
+    // check the file size
+    if (thumbnail.size > 2000000) {
+      return next(
+        new HttpError("Thumbnail too big. File should be less than 2MB."),
+      );
+    }
+
+    const fileName = thumbnail.name;
+    const splittedFilename = fileName.split(".");
+    const newFilename =
+      splittedFilename[0] +
+      uuid() +
+      "." +
+      splittedFilename[splittedFilename.length - 1];
+
+    thumbnail.mv(
+      path.join(__dirname, "..", "uploads", newFilename),
+      async (err) => {
+        if (err) {
+          return next(new HttpError(err));
+        }
+
+        const newPost = await Post.create({
+          title,
+          category,
+          description,
+          thumbnail: newFilename,
+          creator: req.user.id,
+        });
+
+        if (!newPost) {
+          return next(new HttpError("Post couldn't be created.", 422));
+        }
+
+        // find user and increase post count by 1
+        const currentUser = await User.findById(req.user.id);
+        const userPostCount = currentUser.posts + 1;
+
+        await User.findByIdAndUpdate(req.user.id, {
+          posts: userPostCount,
+        });
+
+        res.status(201).json(newPost);
+      },
+    );
+  } catch (error) {
+    return next(new HttpError(error));
+  }
 };
 
 // ============ GET ALL POSTS ==============
